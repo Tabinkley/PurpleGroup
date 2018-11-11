d4eb2042a7bee17a01a6d241f570652f1eb1d140
Tabinkley <44717751+Tabinkley@users.noreply.github.com>
2018-11-11 13:55:07 -0600

    Add files via upload

---
+++ includes/dbh.inc.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Purple Group Project v1.2
+View Admin Page Moudule v1.0
+
+Programers:
+Tabitha Binkley
+Tyson Cruz
+Mathew McSpadden
+
+last updated 11/9/2018
+
+This module is a system for registering and logining in as a user by default but also allows for an admin with privleges such as viewing
+what users have registered with this system, posting blogs that viewers can see and editing and deleteing posts.
+*/
+/*
+This file connects to the server and the database we are using.
+*/
+
+$dBServername = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "purple";
+
+$conn = mysqli_connect($dBServername, $dBUsername, $dBPassword, $dBName);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+date_default_timezone_set("America/Chicago");
