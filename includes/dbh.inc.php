2bb1ff8f0c6554ea4e9a9fbdb9a5b27320e21e66
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-11-11 16:21:25 -0700

    wk3

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -12,8 +12,7 @@ last updated 11/11/2018
 
 This module is a system for registering and logining in as a user by default but also allows for an admin with privleges such as viewing
 what users have registered with this system, posting blogs that viewers can see and editing and deleteing posts.
-*/
-/*
+
 This file connects to the server and the database we are using.
 */
 
