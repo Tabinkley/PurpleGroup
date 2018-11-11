20c7318783878388ae6df101a1147c1d359c7ea5
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-11-11 16:28:23 -0700

    wk3

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 /*
-Purple Group Project v1.1
-View Blog Moudule v1.0
+Purple Group Project v1.3
+Module v3.0
 
 Programers:
 Tabitha Binkley
 Tyson Cruz
-Mathew McSpadden
+Matthew McSpadden
 
-last updated 11/9/2018
+last updated 11/11/2018
 
-This module is a system for registering users and allowing them to login. It also allows users to enter blogs into a database and veiw them.
-*/
+This module is a system for registering and logining in as a user by default but also allows for an admin with privleges such as viewing
+what users have registered with this system, posting blogs that viewers can see and editing and deleteing posts.
 
-/* This is essentially the home page. Currently it only holds a message telling the user "You are logged in!" if there
+This is essentially the home page. Currently it only holds a message telling the user "You are logged in!" if there
 user session started and saying "You are logged out!" if there is no user session started.
 */
 
