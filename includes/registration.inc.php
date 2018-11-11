691617c8f9780e7622e0917f328a4f31429fad46
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-11-11 16:12:58 -0700

    wk3

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,14 +1,14 @@
 <?php
 /*
-Purple Group Project v1.1
+Purple Group Project v1.3
 View Blog Moudule v1.0
 
 Programers:
 Tabitha Binkley
 Tyson Cruz
-Mathew McSpadden
+Matthew McSpadden
 
-last updated 11/9/2018
+last updated 11/11/2018
 
 This module is a system for registering users and allowing them to login. It also allows users to enter blogs into a database and veiw them.
 */
