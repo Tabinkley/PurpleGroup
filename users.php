587ead9498d30d175762960d389bfef9a9353dfd
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-11-11 16:32:21 -0700

    wk3

--- users.php
+++ users.php
@@ -1,4 +1,22 @@
 <?php
+
+/*
+Purple Group Project v1.3
+Module v3.0
+
+Programers:
+Tabitha Binkley
+Tyson Cruz
+Matthew McSpadden
+
+last updated 11/11/2018
+
+This module is a system for registering and logining in as a user by default but also allows for an admin with privleges such as viewing
+what users have registered with this system, posting blogs that viewers can see and editing and deleteing posts.
+
+This page shows all the users for an admin.
+*/
+
 require 'header.php';
 require 'includes/dbh.inc.php';
 
