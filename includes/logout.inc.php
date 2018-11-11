bf5d9f8924207c8b0d574ca5ad99f5fc66f65ddc
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-11-11 16:21:44 -0700

    wk3

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -11,9 +11,9 @@ Matthew McSpadden
 last updated 11/11/2018
 
 This module is a system for registering and logining in as a user by default but also allows for an admin with privleges such as viewing
-what users have registered with this system, posting blogs that viewers can see and editing and deleteing posts.*/
+what users have registered with this system, posting blogs that viewers can see and editing and deleteing posts.
 
-/* This file ends the session started by the login page and sends the user back to the home page.
+This file ends the session started by the login page and sends the user back to the home page.
 */
 session_start();
 session_unset();
