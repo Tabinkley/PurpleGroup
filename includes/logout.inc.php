4e33e0b412ee27e71e97bb5f2aa7efb9b0705f32
mcspadden <34407450+mcspadden@users.noreply.github.com>
2018-11-11 16:20:39 -0700

    wk3

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Purple Group Project v1.3
-View Blog Moudule v1.0
+Module v3.0
 
 Programers:
 Tabitha Binkley
@@ -10,8 +10,8 @@ Matthew McSpadden
 
 last updated 11/11/2018
 
-This module is a system for registering users and allowing them to login. It also allows users to enter blogs into a database and veiw them.
-*/
+This module is a system for registering and logining in as a user by default but also allows for an admin with privleges such as viewing
+what users have registered with this system, posting blogs that viewers can see and editing and deleteing posts.*/
 
 /* This file ends the session started by the login page and sends the user back to the home page.
 */
